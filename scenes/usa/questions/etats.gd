9e551ac85f58bf921a9b3bda8bdb88af6843f5e2
leanecharpentier <leanecharpentierpro@outlook.com>
2024-01-25 19:32:12 +0100

    merge bug fix

--- scenes/usa/questions/etats.gd
+++ scenes/usa/questions/etats.gd
@@ -33,16 +33,9 @@ func _on_d_55_pressed():
 func _on_retour_pressed():
 	global.bonus = false
 	get_tree().change_scene_to_file("res://scenes/home.tscn")
-
+	
 func _on_loose_sound_finished():
-<<<<<<< HEAD
-	get_tree().change_scene_to_file("res://scenes/usa/level_usa.tscn")
-
-func _on_win_sound_finished():
-	get_tree().change_scene_to_file("res://scenes/usa/level_usa.tscn")
-=======
 	get_tree().change_scene_to_file("res://scenes/usa/main.tscn")
 
 func _on_win_sound_finished():
 	get_tree().change_scene_to_file("res://scenes/usa/main.tscn")
->>>>>>> 74c6e69 (sound questions)
